a563b0dc9e82d2d96cb95bc06736d845a7218a14
cookiezbitz <remiellin0.0@gmail.com>
2023-09-11 13:54:18 +0800

    finissshed lab

---
+++ AP COMPUTER SCIENCE A/03_variables_datatypes_labs/Variables.java
@@ -0,0 +1,48 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the
+        //following data types
+        //byte        short        int         long
+        //float        double
+        //char      boolean        String
+
+        //integer variables
+        byte byteOne = 127;
+        short shorty = 2;
+        int inty = 3;
+        long longy = 2133213214;
+        float floaty = 312421412.12212f;
+        double dubby = 124124.1287345747842;
+        char vex4253W = 'w';
+        boolean thisStatementIsFalse = true;
+        String stringies = "stringy twingy wingie, dwingle tringle";
+
+        //decimal variables
+        //other integer types
+        //other types
+        //output your information here
+        System.out.println("/////////////////////////////////");
+        System.out.println("*                               *");
+        System.out.println("*        integer types          *");
+        System.out.println("*                               *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+        System.out.println("*16 bit - shorty = "+shorty+"\t\t*");
+        System.out.println("*32 bit - inty = "+inty+"\t\t*");
+        System.out.println("*64 bit - longy = "+longy+"\t\t*");
+        System.out.println("*        \" real \" types        *");
+        System.out.println("*32 bit - floaty = "+floaty+"\t\t*");
+        System.out.println("*64 bit - dubby = "+dubby+"\t\t*");
+        System.out.println("*        other int types        *"); 
+        System.out.println("*16 bit - vex4253W = "+vex4253W+"\t\t*");
+        System.out.println("*        other types        *"); 
+        System.out.println("* thisStatementIsFalse = "+thisStatementIsFalse+"\t\t*");
+        System.out.println("* stringies = "+stringies+"\t\t*");
+
+    }
+}
