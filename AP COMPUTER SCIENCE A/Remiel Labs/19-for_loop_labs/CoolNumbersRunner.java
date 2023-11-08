84e4b4f68eb4156e00322f691f15d3b96c13f98f
Remiel Lin <74459428+cookiezbitz@users.noreply.github.com>
2023-11-08 13:11:23 +0800

    eeee

---
+++ AP COMPUTER SCIENCE A/Remiel Labs/19-for_loop_labs/CoolNumbersRunner.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name
+
+public class CoolNumbersRunner
+{
+	public static void main( String[] args )
+	{
+		System.out.println( CoolNumbers.countCoolNumbers(250) + " cool numbers between 6 - " + 250);
+		//add more test cases
+	}
+}
---
+++ AP COMPUTER SCIENCE A/Remiel Labs/19-for_loop_labs/CountPairs.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name
+
+public class CountPairs
+{
+	public static int pairCounter( String str )
+	{ 
+		int count = 0;
+		return count;
+	}
+}
